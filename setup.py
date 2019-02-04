0195eff4972dd5ef65c050e312f5677cda0617ec
awalker <awalker@ixsystems.com>
2019-02-04 07:34:59 -0500

    Initial import

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='wsdd',
+      version='1.0',
+      packages=['wsdd'],
+      package_dir={'wsdd': 'src'},
+      author='Andrew Walker',
+      author_email='awalker@ixsystems.com',
+      url='https://github.com/freenas/wsdd'
+      )
