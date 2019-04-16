987d9dc149adad26c56deef34eff818388f8317b
awalker <awalker@ixsystems.com>
2019-04-16 09:06:45 -0400

    Bring in latest changes from upstream and fix installer

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
-from distutils.core import setup
-setup(name='wsdd',
-      version='1.0',
-      packages=['wsdd'],
-      package_dir={'wsdd': 'src'},
-      author='Steffen Christgau',
-      license='MIT',
-      url='https://github.com/freenas/wsdd'
+import setuptools
+
+setuptools.setup(
+    name='wsdd',
+    version='1.1',
+    author='Steffen Christgau',
+    description="Web Service Discovery host daemon",
+    packages=setuptools.find_packages(),
+    scripts=["src/wsdd.py"],
+    license='MIT',
+    platforms="any",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: No Input/Output (Daemon)",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3",
+    ],
+    url='https://github.com/freenas/wsdd'
 )
--- src/wsdd.py
+++ src/wsdd.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env %%PYTHON3_CMD%% 
+#!/usr/bin/env python3 
 
 # Implements a target service according to the Web Service Discovery
 # specification.
